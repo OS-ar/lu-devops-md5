ede2e7327ee9e3e333c5278ed94b79c87633d82b
OS-ar <arturs.ostapenko90@gmail.com>
2022-05-18 19:46:29 +0300

    tf edit

--- tf/main.tf
+++ tf/main.tf
@@ -1,34 +1,41 @@
 provider "aws" {
   region = var.region
 }
-resource "aws_key_pair" "ao-tf-pub" {
-  key_name   = "aws_key"
-  public_key = "${file("aws_key.pub")}"
-}
+# resource "aws_key_pair" "ao-tf-pub" {
+#   key_name   = "ao_key_pair"
+#   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0MJ2IZSmowimybBxhjK9o3e7dbDfnjbgkoWacyL1+L"
+# }
 resource "aws_instance" "ansible-hosts" {
   # Creates four identical aws ec2 instances
-  count = 2     
-  
-  ami = var.inst_type
-  instance_type = var.instance_type
-  key_name = aws_key_pair.ao-tf-pub
+  count = "2"     
+  ami = var.ami_id
+  instance_type = var.inst_type
+  key_name = "ao_key_pair"
   # Create ansible user 
   user_data = "${file("cloud_init.sh")}" 
   tags = {
     # The count.index allows you to launch a resource 
     # starting with the distinct index number 0 and corresponding to this instance.
-    Name = "ansible-host-${count.index}"
+    Name = "grp2-ao-ansible-host-${count.index}"
   }
 }
 resource "aws_instance" "ansible-master" {
       
   # All instances will have the same ami and instance_type
-  ami = var.inst_type
-  instance_type = var.instance_type 
-  key_name = aws_key_pair.ao-tf-pub 
+  ami = var.ami_id
+  instance_type = var.inst_type
+  key_name = "ao_key_pair"
+  user_data = <<EOF
+#!/bin/bash 
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+hostnamectl set-hostname master-arturs-os
+EOF 
   tags = {
     # The count.index allows you to launch a resource 
     # starting with the distinct index number 0 and corresponding to this instance.
-    Name = "ansible-host-${count.index}"
-  }
+    Name = "grp2-ao-ansible-master"
+    }
 }
--- tf/vars.tf
+++ tf/vars.tf
@@ -2,6 +2,11 @@ variable "inst_type" {
   default = "t2.micro"
 }
 variable "region" {
-    default = "us-east-2"
-  
+  default = "us-east-2"
+}
+variable "ami_id" {
+  default = "ami-0aeb7c931a5a61206"
+}
+variable "sec_grp" {
+  default = "sg-0eb6cbd72b0d7c8b0"
 }
